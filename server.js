7a5e2160ce1f09635514d2f54aa7ed1203c82673
Julio Quintana <jquintana1801@gmail.com>
2018-03-06 00:50:57 -0300

    Habilitando cors en api rest

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
-var mongoose = require('mongoose')
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
 
 const routes = require('./routes')
 
